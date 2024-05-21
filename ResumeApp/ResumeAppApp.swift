e1e2e4a1ae274003af11c06131e30d8cce4ca462
Ritika Gupta <ritikagupta2020@gmail.com>
2024-05-21 11:56:37 +0530

    Initial Commit

---
+++ ResumeApp/ResumeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeAppApp.swift
+//  ResumeApp
+//
+//  Created by Ritika Gupta on 21/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ResumeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
